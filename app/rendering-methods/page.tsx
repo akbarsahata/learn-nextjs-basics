281b688b284f4ec283a9a76b850573443a25c254
akbarsahata <11588611+akbarsahata@users.noreply.github.com>
2025-07-13 10:20:18 +0700

    refactor: moved rendering methods demo pages with SSR, SSG, and client-rendered components to it's own folder

--- app/rendering-methods/page.tsx
+++ app/rendering-methods/page.tsx
@@ -1,26 +1,32 @@
-import { Separator } from "@/lib/ui/common";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="border border-gray-300 rounded-xl p-8 flex flex-col gap-4 bg-white min-w-[300px] shadow-md">
-        <h1 className="font-extrabold text-3xl text-red-600 text-center animate-bounce mb-2">
-          Next.JS Basics
-        </h1>
-        <Separator />
         <Link
-          href="/rendering-methods"
+          href="rendering-methods/ssr"
           className="py-3 rounded-lg border border-gray-200 text-center no-underline text-gray-800 bg-gray-50 hover:bg-gray-100 transition"
         >
-          Rendering Methods
+          SSR
         </Link>
-        <Separator />
         <Link
-          href="/forms"
+          href="rendering-methods/ssg"
           className="py-3 rounded-lg border border-gray-200 text-center no-underline text-gray-800 bg-gray-50 hover:bg-gray-100 transition"
         >
-          Forms
+          SSG
+        </Link>
+        <Link
+          href="rendering-methods/isr"
+          className="py-3 rounded-lg border border-gray-200 text-center no-underline text-gray-800 bg-gray-50 hover:bg-gray-100 transition"
+        >
+          ISR
+        </Link>
+        <Link
+          href="rendering-methods/client-rendered"
+          className="py-3 rounded-lg border border-gray-200 text-center no-underline text-gray-800 bg-gray-50 hover:bg-gray-100 transition"
+        >
+          Client Rendered
         </Link>
       </div>
     </div>
